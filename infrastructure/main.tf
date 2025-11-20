024a6d70d488006b435ceb8e94d6d12bbf653187
Edward Hallam <edwardhallam@gmail.com>
2025-11-19 16:12:49 -0800

    Delete infrastructure directory

--- infrastructure/main.tf
+++
@@ -1,9 +0,0 @@
-# Main Terraform Configuration
-#
-# This file will contain the module calls for your services.
-# Example:
-#
-# module "test_service" {
-#   source = "./modules/launch-service"
-#   ...
-# }
